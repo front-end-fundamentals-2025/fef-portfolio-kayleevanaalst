8cffcb304780039ac91401b6ac8b42b1362ccd64
kayleevanaalst <vaka23lt@student.ju.se>
2025-02-20 16:10:53 +0100

    dark and light mode

--- button.js
+++ button.js
@@ -1,24 +1,29 @@
 const themeToggleButton = document.getElementById('theme-toggle');
-const body = document.body();
+const body = document.body;
 
 if (localStorage.getItem('theme') === 'dark') {
     body.classList.add('dark-theme');
-    themeToggleButton.textContent = 'Switch to Light Mode';
+    themeToggleButton.textContent = 'Light Mode';
 } else {
     body.classList.add('light-theme');
-    themeToggleButton.textContent = 'Switch to Dark Mode';
+    themeToggleButton.textContent = 'Dark Mode';
 }
 
 themeToggleButton.addEventListener('click', () => {
     if (body.classList.contains('light-theme')) {
         body.classList.remove('light-theme');
         body.classList.add('dark-theme');
-        themeToggleButton.textContent = 'Switch to Light Mode';
+        themeToggleButton.textContent = 'Light Mode';
         localStorage.setItem('theme', 'dark');
     } else {
         body.classList.remove('dark-theme');
         body.classList.add('light-theme');
-        themeToggleButton.textContent = 'Switch to Dark Mode';
+        themeToggleButton.textContent = 'Dark Mode';
         localStorage.setItem('theme', 'light');
     }
+});
+
+themeToggleButton.addEventListener('click', () => {
+    console.log('Button clicked!');
+    console.log(body.classList);
 });
